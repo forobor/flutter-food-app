b081c63a1cebf66cc9ea91b088c3996f14ef1a17
Demid Ost <demostapenko@gmail.com>
2019-01-26 16:14:26 +0200

    update

---
+++ lib/pages/product_edit.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/helpers/ensure-visible.dart';
+
+class ProductEditPage extends StatefulWidget {
+  final Function addProduct;
+  final Function updateProduct;
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductEditPage(
+      {this.addProduct, this.updateProduct, this.product, this.productIndex});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductEditPage();
+  }
+}
+
+class _ProductEditPage extends State<ProductEditPage> {
+  final Map<String, dynamic> _formData = {
+    'title': null,
+    'price': null,
+    'description': null,
+    'imageUrl': 'assets/food.jpg'
+  };
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _titleFocusNode = FocusNode();
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    if (widget.product == null) {
+      widget.addProduct(_formData);
+    } else {
+      widget.updateProduct(widget.productIndex, _formData);
+    }
+    _formKey.currentState.save();
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double listPadding =
+        MediaQuery.of(context).orientation == Orientation.landscape
+            ? 60.0
+            : 0.0;
+
+    final Widget _main = GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: listPadding),
+            children: <Widget>[
+              EnsureVisibleWhenFocused(
+                focusNode: _titleFocusNode,
+                child: TextFormField(
+                  focusNode: _titleFocusNode,
+                  decoration: InputDecoration(labelText: 'Product Title'),
+                  initialValue:
+                      widget.product != null ? widget.product['title'] : '',
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Title is requiered.';
+                    }
+                  },
+                  onSaved: (String value) {
+                    _formData['title'] = value;
+                  },
+                ),
+              ),
+              EnsureVisibleWhenFocused(
+                focusNode: _priceFocusNode,
+                child: TextFormField(
+                  focusNode: _priceFocusNode,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: 'Price'),
+                  initialValue: widget.product != null
+                      ? widget.product['price'].toString()
+                      : '',
+                  validator: (String value) {
+                    if (value.isEmpty ||
+                        !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$')
+                            .hasMatch(value)) {
+                      return 'Price is requiered and should be a number.';
+                    }
+                  },
+                  onSaved: (String value) {
+                    _formData['price'] = double.parse(value);
+                  },
+                ),
+              ),
+              EnsureVisibleWhenFocused(
+                focusNode: _descriptionFocusNode,
+                child: TextFormField(
+                  focusNode: _descriptionFocusNode,
+                  decoration: InputDecoration(labelText: 'Description'),
+                  initialValue: widget.product != null
+                      ? widget.product['description']
+                      : '',
+                  maxLines: 4,
+                  onSaved: (String value) {
+                    _formData['description'] = value;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: RaisedButton(
+                  child: Text('Save'),
+                  color: Theme.of(context).accentColor,
+                  textColor: Colors.white,
+                  onPressed: _submitForm,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    return widget.product == null
+        ? _main
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Edit Prodcut'),
+            ),
+            body: _main);
+  }
+}
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
+import './product_edit.dart';
+
 class ProductListPage extends StatelessWidget {
+  final List<Map<String, dynamic>> products;
+  final Function updateProduct;
+
+  ProductListPage(this.products, this.updateProduct);
+
+  Widget _buildListItem(BuildContext context, int index) {
+    return ListTile(
+      leading: Image.asset(
+        products[index]['imageUrl'],
+        height: 50.0,
+        width: 50.0,
+      ),
+      title: Text(products[index]['title']),
+      trailing: IconButton(
+        icon: Icon(Icons.edit),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return ProductEditPage(product: products[index], updateProduct: updateProduct, productIndex: index,);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('All products'),
+    return ListView.builder(
+      itemBuilder: _buildListItem,
+      itemCount: products.length,
     );
   }
 }
